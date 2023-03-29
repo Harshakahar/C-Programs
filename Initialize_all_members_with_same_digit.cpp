ea67919ade0a77eb94170a7dc16c9570cf3b22f1
Harshakahar <107496326+Harshakahar@users.noreply.github.com>
2023-03-29 21:57:36 +0530

    Add files via upload

---
+++ Initialize_all_members_with_same_digit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int arr[10];
+    for(int i=0;i<10;i++)
+        arr[i]=1;
+    for(int i=0;i<9;i++)
+        cout<<arr[i]<<endl;
+    return 0;
+}
